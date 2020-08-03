552208661f6e08aa6d67ca8b5d4e4e5a6a5fe4d9
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-03 01:51:10 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,4 +16,11 @@ class StudentsController < ApplicationController
     session[:form_params] = params.inspect
     redirect_to new_student_path
   end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
 end
